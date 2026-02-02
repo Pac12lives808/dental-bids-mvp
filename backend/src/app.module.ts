84231fbd47b7e343b853600f6189c73e4ae59cc2
Pac12lives808 <ryansalanoa16@gmail.com>
2026-02-01 22:26:11 -0700

    Create app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { CasesModule } from './cases/cases.module';
+import { BidsModule } from './bids/bids.module';
+import { StripeModule } from './stripe/stripe.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    CasesModule,
+    BidsModule,
+    StripeModule,
+  ],
+})
+export class AppModule {}
